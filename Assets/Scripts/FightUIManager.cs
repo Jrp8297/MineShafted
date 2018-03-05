be11bd256a34ebc293636c2ecb9493f5df600b17
Sean Koh (RIT Student) <pxk8689@ad.rit.edu>
2018-03-05 16:29:23 -0500

    Merging fight and shaft scene

--- Assets/Scripts/FightUIManager.cs
+++ Assets/Scripts/FightUIManager.cs
@@ -5,28 +5,43 @@ using UnityEngine.SceneManagement;
 public class FightUIManager : MonoBehaviour {
     public Canvas defeatPanel;
     public Canvas victoryPanel;
+    public Canvas fightScene;
     bool afterFight = false;
-    public PlayerScript player;
+    public bool inFight = false;
 	// Use this for initialization
 	void Start ()
     {
-		
+        inFight = gameObject.GetComponent<ShaftObject>().inFight;
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        
+        if(inFight)
+        {
+            fightScene.enabled = true;
+        }
 	}
 
     public void Defeat()
     {
+        inFight = false;
+        fightScene.enabled = false;
         SceneManager.LoadScene("UpgradeScene");
-        
-        
+       
+
+
     }
     public void Victory()
     {
-        SceneManager.LoadScene("ShaftScene");       
+        fightScene.enabled = false;
+        inFight = false;
+        
+        
     }
+    public void InFight()
+    {
+        inFight = true;
+    }
+
 }
